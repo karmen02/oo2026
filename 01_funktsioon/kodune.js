56a3155e4f817a42ee724878b7f26c8fcc514e3b
karmen02 <233921595+karmen02@users.noreply.github.com>
2026-02-10 13:19:22 +0200

    Homework - Net salary calculation

--- 01_funktsioon/kodune.js
+++ 01_funktsioon/kodune.js
@@ -1,8 +1,10 @@
 //Calculating net vs gross salary via functions
 //Gross salary is before any deductions, net is after deductions.  
-//This is not taking into account the minimum salary income which is 700EUR per month and the pension contributions. 
-//Using typical estonian tax rates for calculations. These are: income tax of 20%, social tax of 33% and unemployment insurance of 1.6%.
-//As taxes unchanging in time, except when the gov. decides to change them, theyre being kept as constants.
+//The program does not take into account monthly pension contributions.
+//The program does take into account a monthly tax free sum of 700€. 
+//Using typical estonian tax rates for calculations. These are: income tax of 22% and unemployment insurance of 1.6%.
+//As taxes are unchanging in time, except when the gov. decides to change them, theyre being kept as constants.
+//Not meant for salaries over 1200EUR per month.
 var unemploymentInsurance = 0.016;
 var incomeTax = 0.22;
 var taxFree = 700;
@@ -14,7 +16,7 @@ function salaryNet(grossSalary) {
     //calculating the income tax with the included tax free basis of 700EUR
     var taxedSalary = taxedSum - taxFree;
     //Telling the program that, if after dividing the initial taxed wage (that already has unemployment insurance calculated off of it)
-    //we still have money left over, it should be further taxed with income tax. As well as if the current taxed Salary is smaller than 0,
+    //we still have money left over, it should be further taxed with income tax. As well as if the current taxed salary is smaller than 0,
     //that for further calculations it should be zero.
     if (taxedSalary < 0) {
         taxedSalary = 0;
@@ -24,13 +26,28 @@ function salaryNet(grossSalary) {
     var salaryNet = grossSalary - unemploymentInsuranceAmount - incomeTaxAmount;
     return Math.round(salaryNet * 100) / 100;
 }
+var spacing = "*******************";
 //Initial gross salary to show how the program works. 
 var grossSalary = 2000;
 var netWage = salaryNet(grossSalary);
 console.log("If gross salary is " + grossSalary + "EUR. Then the net salary would be " + netWage + "EUR. ");
+console.log(spacing);
 //These are only to show net salaries for general salaries.
+var message = "More examples of gross salaries per month and their comparisons to net salaries. Keep in mind this is an example and the math changes with an income that's over 7848 EUR per year(1200 EUR per month).";
+console.log(message);
 var salaries = [900, 1100, 1300, 1500, 1700, 1900, 2100, 2300, 2500, 2900, 3100];
 for (var _i = 0, salaries_1 = salaries; _i < salaries_1.length; _i++) {
     var salary = salaries_1[_i];
-    console.log(salaryNet(salary));
+    console.log(salary + " EUR =  " + salaryNet(salary) + "EUR");
 }
+console.log(spacing);
+var message1 = "Showing gross salaries with intervals of 150EUR, up to 1200 EUR, and their respective net salaries.";
+console.log(message1);
+var salaries2 = [];
+for (var grossSalary_1 = 300; grossSalary_1 <= 1200; grossSalary_1 += 50) {
+    salaries2.push([
+        grossSalary_1 + " EUR",
+        salaryNet(grossSalary_1) + " EUR"
+    ]);
+}
+console.log(salaries2);
--- 01_funktsioon/kodune.ts
+++ 01_funktsioon/kodune.ts
@@ -1,9 +1,10 @@
 //Calculating net vs gross salary via functions
 //Gross salary is before any deductions, net is after deductions.  
-//This is not taking into account the minimum salary income which is 700EUR per month and the pension contributions. 
-//Using typical estonian tax rates for calculations. These are: income tax of 20%, social tax of 33% and unemployment insurance of 1.6%.
-
-//As taxes unchanging in time, except when the gov. decides to change them, theyre being kept as constants.
+//The program does not take into account monthly pension contributions.
+//The program does take into account a monthly tax free sum of 700€. 
+//Using typical estonian tax rates for calculations. These are: income tax of 22% and unemployment insurance of 1.6%.
+//As taxes are unchanging in time, except when the gov. decides to change them, theyre being kept as constants.
+//Not meant for salaries over 1200EUR per month.
 const unemploymentInsurance: number = 0.016;
 const incomeTax: number = 0.22;
 const taxFree: number = 700;
@@ -15,7 +16,7 @@ function salaryNet(grossSalary: number): number {
     //calculating the income tax with the included tax free basis of 700EUR
     let taxedSalary = taxedSum - taxFree;
     //Telling the program that, if after dividing the initial taxed wage (that already has unemployment insurance calculated off of it)
-    //we still have money left over, it should be further taxed with income tax. As well as if the current taxed Salary is smaller than 0,
+    //we still have money left over, it should be further taxed with income tax. As well as if the current taxed salary is smaller than 0,
     //that for further calculations it should be zero.
     if (taxedSalary < 0){
         taxedSalary = 0;
@@ -26,15 +27,34 @@ function salaryNet(grossSalary: number): number {
     return Math.round(salaryNet * 100) / 100;
 }
 
+let spacing:string="*******************"
+
 //Initial gross salary to show how the program works. 
 let grossSalary: number = 2000;
 let netWage: number = salaryNet(grossSalary);
 console.log("If gross salary is " + grossSalary + "EUR. Then the net salary would be " + netWage + "EUR. ")
 
+console.log(spacing)
 //These are only to show net salaries for general salaries.
+let message:string="More examples of gross salaries per month and their comparisons to net salaries. Keep in mind this is an example and the math changes with an income that's over 7848 EUR per year(1200 EUR per month)."
+console.log(message)
+
 let salaries: number[]=[900, 1100, 1300, 1500, 1700, 1900, 2100, 2300, 2500, 2900, 3100]
 
 for(let salary of salaries){
-    console.log(salaryNet(salary));
+    console.log(salary + " EUR =  " + salaryNet(salary) + "EUR");
 }
 
+console.log(spacing)
+
+let message1:string="Showing gross salaries with intervals of 150EUR, up to 1200 EUR, and their respective net salaries."
+console.log(message1)
+
+let salaries2: (number | string) [][]=[]
+for(let grossSalary=300; grossSalary<=1200; grossSalary+=50){
+    salaries2.push([
+        grossSalary + " EUR", 
+        salaryNet(grossSalary) + " EUR"
+    ])
+}
+console.log(salaries2)
